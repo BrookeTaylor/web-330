56379eb3db57c554c0fe556eeaa974542531b42c
Brooke Taylor <anonyoumiss@gmail.com>
2023-01-08 15:41:36 -0600

    Adding first assignment.

---
+++ theme.js
@@ -0,0 +1,30 @@
+/*
+============================================================
+; Title: theme.js
+; Author: Professor Krasso
+; Date: 22 March 2021
+; Description: An example program that demonstrates how to switch 
+; website themes using JavaScript.
+;============================================================
+*/
+
+/**
+ * Set the default theme to light if one has not been set in the browsers localStorage
+ */
+function setDefaultTheme() {
+
+  const theme = localStorage.getItem("mode") || "light-theme";
+  const iconMode = localStorage.getItem("iconMode") || "fa-toggle-off";
+  const iconText = localStorage.getItem("iconText") || "Light Mode";
+
+  document.body.classList.value = theme;
+  document.getElementById("icon-mode").classList.add(iconMode);
+  document.getElementById("icon-text").innerHTML = iconText;
+}
+
+/**
+ * Set the HTML body to the user's selected theme. If one has not been selected, set the theme to light-theme
+ */
+function setSelectedTheme() {
+  document.body.classList.value = localStorage.getItem("mode") || "light-theme";
+}
